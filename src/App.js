3dd90c9431827d7be6f84b8bdf4cacc009c87dc7
khulysojohn <khuliso.thavhiwa@techbridlefoundation.org>
2024-12-17 19:10:21 +0200

    header updated

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 
 import './App.css';
 import Header from './componets/header';
-import Home from './componets/home';
-import About from './componets/about';
-import Project from './componets/project';
-import Contact from './componets/contact';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Home />
-      <About />
-      <Project />
-      <Contact />
+    
     </div>
   );
 }
--- src/componets/header.jsx
+++ src/componets/header.jsx
@@ -1,44 +1,60 @@
-import React from "react";
-import logo from "../assets/avata.png"
+import React, { useState } from "react";
+import logo from "../assets/avata.png";
+import { FaBars, FaTimes } from "react-icons/fa";
+import classNames from "classnames";
 
-function Header () {
-    return (
-       
-        <header className="header">
-            <div className="header_content">
-                <div className="logo_container">
-                    <div className="logo_img_cont">
-                        <img src={logo} alt="logo_img" className="logo_img"></img>
-                    </div>
-                    <span className="logo_sub">KhulysoJohn</span>
-                </div>
+const Header = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
 
-                <div className="header_main">
-                    <ul className="main_links">
-                        <li className="link_wrapper">
-                            <a href="./home.jsx" className="nav_link">home</a>
-                        </li>
-                  
-                        <li className="link_wrapper">
-                            <a href="./about.jsx" className="nav_link">about</a>
-                        </li>
-                 
-                         <li className="link_wrapper">
-                            <a href="./project.jsx" className="nav_link">projects</a>
-                        </li>
-                   
-                        <li className="link_wrapper">
-                            <a href="./contact.jsx" className="nav_link">contact</a>
-                        </li>
-                    </ul>
+  return (
+    <div className="header">
+      {/* Logo Container */}
+      <div className="logo_container">
+        <div className="img_cont">
+          <img src={logo} alt="logo-img" className="logo_img" />
+        </div>
+        <span className="logo_sub">KhulysoJohn</span>
+      </div>
 
-                <div className="mobile_menu">
+      {/* Desktop Menu */}
+      <ul className="desktop_menu">
+        <li>
+          <a href="#home" className="nav_link">Home</a>
+        </li>
+        <li>
+          <a href="#about" className="nav_link">About</a>
+        </li>
+        <li>
+          <a href="#project" className="nav_link">Project</a>
+        </li>
+        <li>
+          <a href="#contact" className="nav_link">Contact</a>
+        </li>
+      </ul>
 
-                    </div>
-                </div>
-            </div>
-        </header>
-    );
-}
+      {/* Hamburger Icon */}
+      <div onClick={handleClick} className="hamburger">
+        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+      </div>
+
+      {/* Mobile Menu */}
+      <ul className={classNames("mobile_menu", { hidden: !nav })}>
+        <li className="mobile_nav_link">
+          <a href="#home">Home</a>
+        </li>
+        <li className="mobile_nav_link">
+          <a href="#about">About</a>
+        </li>
+        <li className="mobile_nav_link">
+          <a href="#project">Project</a>
+        </li>
+        <li className="mobile_nav_link">
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default Header;
