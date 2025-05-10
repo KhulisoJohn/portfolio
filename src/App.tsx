0ab9e03757a85e36416e03c574eec9a935edfa17
khulysojohn <khuliso.thavhiwa@techbridlefoundation.org>
2025-05-10 15:16:23 +0200

    new project setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+  
+  return (
+    <>
+     <div className="text-3xl justify-center font-bold">
+      <p className="text-center text-red-500">Hello world.</p>
+    </div>
+    </>
+  )
+}
+
+export default App
