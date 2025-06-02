969e45e8c5c28481ae4b64107dad595d32119d7f
KhulisoJohn <khuliso.thavhiwa@techbridlefoundation.org>
2025-06-02 17:20:16 +0200

    initial commit

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -11,17 +11,21 @@ const skills = [
   { name: "Next.js", level: 80, category: "frontend" },
 
   // Backend
-  { name: "Node.js", level: 80, category: "backend" },
-  { name: "Express", level: 75, category: "backend" },
-  { name: "MongoDB", level: 70, category: "backend" },
-  { name: "PostgreSQL", level: 65, category: "backend" },
-  { name: "GraphQL", level: 60, category: "backend" },
+  { name: "C#", level: 80, category: "backend" },
+  { name: "Python", level: 75, category: "backend" },
+  { name: "Node.js", level: 70, category: "backend" },
+  { name: "mySQL", level: 65, category: "backend" },
+  { name: "MongoDB", level: 60, category: "backend" },
+  { name: "ASP.NET", level: 60, category: "backend" },
+
 
   // Tools
   { name: "Git/GitHub", level: 90, category: "tools" },
   { name: "Docker", level: 70, category: "tools" },
-  { name: "Figma", level: 85, category: "tools" },
+  { name: ".NET", level: 85, category: "tools" },
   { name: "VS Code", level: 95, category: "tools" },
+   { name: "MMSQL Server", level: 85, category: "tools" },
+  { name: "Azure Cloud", level: 95, category: "tools" },
 ];
 
 const categories = ["all", "frontend", "backend", "tools"];
