cefd7ae028ce0f5a53a719db433c8c007e245de7
Khulyso john <khuliso.thavhiwa@techbridlefoundation.org>
2026-07-09 17:33:49 +0200

    backend server initialized

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -11,12 +11,9 @@ export const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
      
-      {/* Background Effects */}
      <StarBackground /> 
 
-      {/* Navbar */}
       <Navbar />
-      {/* Main Content */}
       <main>
         <HeroSection />
         <AboutSection />
